4144a47b4061188e30866c024f809157842b4757
Konstantin Voykov <koykov0@gmail.com>
2021-01-30 21:15:39 +0200

    Remove using total; read current value with atomic-read protection.

--- counter.go
+++ counter.go
@@ -3,9 +3,8 @@ package counter
 import "sync/atomic"
 
 type Counter struct {
-	now   *int64
-	msec  [1000]uint32
-	total uint32
+	now  *int64
+	msec [1000]uint32
 }
 
 var (
@@ -29,16 +28,26 @@ func (c *Counter) Inc() {
 func (c *Counter) Sum() uint32 {
 	var sum uint32
 	for i := 0; i < 1000; i += 10 {
-		sum += (c.msec[i] + c.msec[i+1]) + (c.msec[i+2] + c.msec[i+3]) + (c.msec[i+4] + c.msec[i+5]) + (c.msec[i+6] + c.msec[i+7]) + (c.msec[i+8] + c.msec[i+9])
+		sum += (c.val(i) + c.val(i+1)) + (c.val(i+2) + c.val(i+3)) + (c.val(i+4) + c.val(i+5)) + (c.val(i+6) + c.val(i+7)) + (c.val(i+8) + c.val(i+9))
 	}
-	atomic.StoreUint32(&c.total, sum)
 	return sum
+}
+
+func (c *Counter) StopAll() {
+	done <- true
 }
 
 func (c *Counter) reset(idx int64) {
 	c.msec[idx] = 0
 }
 
-func (c *Counter) StopAll() {
-	done <- true
+func (c *Counter) val(idx int) uint32 {
+	prev := idx - 1
+	if prev < 0 {
+		prev = 999
+	}
+	if *c.now == int64(prev) || *c.now == int64(idx) {
+		return atomic.LoadUint32(&c.msec[idx])
+	}
+	return c.msec[idx]
 }
