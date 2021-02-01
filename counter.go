1f5d510f6a21cfe48393d0b856ac27fb6459e4c0
Konstantin Voykov <koykov0@gmail.com>
2021-02-02 00:03:45 +0200

    Move contents of init.go to the main source file.

--- counter.go
+++ counter.go
@@ -3,6 +3,7 @@ package counter
 import (
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 // Realtime counter.
@@ -68,4 +69,32 @@ func (c *Counter) StopAll() {
 // Reset counter for given millisecond value.
 func (c *Counter) reset(idx int64) {
 	atomic.StoreUint32(&c.msec[idx], 0)
+}
+
+func init() {
+	// Prepare done channel.
+	done = make(chan bool, 1)
+	// Init ticker for milliseconds.
+	tickMsec := time.NewTicker(time.Millisecond)
+	go func() {
+		for {
+			select {
+			case <-tickMsec.C:
+				// New millisecond came.
+				// Update global now with current millisecond.
+				now := (time.Now().UnixNano() / 1e6) % 1000
+				atomic.StoreInt64(&globNow, now)
+
+				mux.RLock()
+				for i := 0; i < len(registry); i++ {
+					// Reset counter of millisecond for each counter.
+					registry[i].reset(now)
+				}
+				mux.RUnlock()
+			case <-done:
+				// Done signal caught, exiting.
+				return
+			}
+		}
+	}()
 }
